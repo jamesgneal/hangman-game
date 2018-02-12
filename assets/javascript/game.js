f00c80df470b0120fb465d97b55654cadf36059b
James Neal <jamesgneal@Jamess-MacBook-Pro.local>
2018-02-12 17:56:41 -0500

    pseudo code for layouts and functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// link to the html page
+
+// on page load:
+    // display instructions and "press enter to start"
+
+// press enter:
+    // randomly select from the array of game words
+    // display game area
+
+// on a key event:
+    // read which key was pressed
+    // loop to compare key value to each index position of the word array
+    // reduce number of guesses remaining
+    // if correct, reveal position in 
