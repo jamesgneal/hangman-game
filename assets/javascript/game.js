fcd679163e9afbe660977aec73abe457d8ccef39
James Neal <jamesgneal@jamess-mbp.lan>
2018-02-16 16:40:40 -0500

    remove typo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -147,7 +147,7 @@ $(document).ready(function() {
 
             // Reveal the word just guessed at the bottom of the screen
             document.querySelector("#right-or-wrong").innerHTML = "SORRY - YOU COULDN'T GUESS";
-            document.querySelector("#last-word").innerHTML = activeWord.join(" ") + "!";
+            document.querySelector("#last-word").innerHTML = activeWord.join(" ");
             // document.querySelector("#game-image").innerHTML = '<img src="assets/images/baselayer.png">';
 
             // reset the game area
